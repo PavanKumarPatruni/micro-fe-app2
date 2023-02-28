1e478eace10d77aebb0d4b1fb70741b5de45f23c
PavanKumarPatruni <pavanpathro@gmail..com>
2023-02-28 13:27:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+const MFA1_Button = React.lazy(() => import("mfa1/Button"));
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Micro FE 2</h1>
+      <React.Suspense fallback="Loading Button">
+        <MFA1_Button />
+      </React.Suspense>
     </div>
   );
 }
